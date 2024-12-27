6246a27f34d24dd24ee2e3e39ce203c14db0969e
Whu00 <idshekunov@gmail.com>
2024-12-27 16:33:55 +0500

    Initial commit

---
+++ src/client/java/net/whu/ctotem/client/CtotemClient.java
@@ -0,0 +1,10 @@
+package net.whu.ctotem.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CtotemClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/whu/ctotem/Ctotem.java
@@ -0,0 +1,10 @@
+package net.whu.ctotem;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ctotem implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
